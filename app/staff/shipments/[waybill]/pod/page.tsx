df4b8bd13bfb59e224a5fe584747387c852ce4c6
Kapilla Dev <dev@kapilla.com>
2026-01-28 00:38:03 +0300

    fix: adjust waybill and pod print styles for mobile single page layout

--- app/staff/shipments/[waybill]/pod/page.tsx
+++ app/staff/shipments/[waybill]/pod/page.tsx
@@ -78,7 +78,7 @@ export default function ProofOfDeliveryPage() {
       </button>
 
       {/* A4 Paper Container */}
-      <div className="bg-white w-[210mm] min-h-[297mm] p-[20mm] shadow-2xl print:shadow-none print:w-full print:h-full box-border relative text-black">
+      <div className="bg-white w-[210mm] min-h-[297mm] p-[20mm] shadow-2xl print:shadow-none print:w-[210mm] print:h-auto print:min-h-0 box-border relative text-black mx-auto">
         
         {/* Header */}
         <div className="flex justify-between items-start border-b-2 border-black pb-6 mb-8">
